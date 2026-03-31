0d59dc00f0548cd65ccc992d0d953577b1c4ba84
Alina Davydenko <Alinadavydenko620@gmail.com>
2026-03-31 12:37:08 +0300

    add emojies

--- some_test_attempts.py
+++ some_test_attempts.py
@@ -1,13 +1,5 @@
-from core.sql_core.sql_statistics import ScoreStatistics
-from core.sql_core.connect_to_host import db
+from core.utils.randomizer import get_random_user
+from core.dictionaries.emojie_dict import emojie
 
-db.connect()
-score_statistics = ScoreStatistics(db)
-all_stat_list = score_statistics.get_all_statistics()
-
-# print(all_stat_list)
-db.disconnect()
-
-# Get stat
-for element in all_stat_list:
-    print(f'{element[0]}, {element[1]}, {element[2]}')
+a = get_random_user(emojie)
+print(a)
