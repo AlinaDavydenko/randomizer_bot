5af31704c8a537293d03825aedc0c93557e8eb66
Alina Davydenko <Alinadavydenko620@gmail.com>
2026-04-06 13:23:51 +0300

    fix with sql core

--- core/sql_core/sql_statistics.py
+++ core/sql_core/sql_statistics.py
@@ -6,7 +6,7 @@ class ScoreStatistics:
     def __init__(self, db_connection):
         self.db = db_connection
 
-    def get_all_statistics(self):
+    def get_total_statistics(self, group_id):
         """Get all statistics"""
         try:
             with self.db.conn.cursor() as cur:
@@ -14,23 +14,21 @@ class ScoreStatistics:
                 cur.execute(
                     """
                     select user_id, SUM(point) as sum_point from scores
+                    where group_id = %s
                     group by user_id
                     order by sum_point desc;
-                    """
+                    """, (group_id, ), 
                 )
 
                 rows = cur.fetchall()
-                all_stat_list = []
-                for row in rows:
-                    all_stat_list.append(row)
 
-                return all_stat_list
+                return rows
 
         except Exception as e:
             print(f"Error get_all_users: {e}")
             return []
 
-    def get_year_statistics(self):
+    def get_year_statistics(self, group_id):
         """Get all statistics"""
         try:
             with self.db.conn.cursor() as cur:
@@ -38,25 +36,22 @@ class ScoreStatistics:
                 cur.execute(
                     """
                     SELECT user_id, SUM(point) as sum_point FROM scores
-                    WHERE EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE)
+                    WHERE group_id = %s and EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE)
                     GROUP BY user_id
                     ORDER BY sum_point DESC
                     LIMIT 5;
-                    """
+                    """, (group_id, ), 
                 )
 
                 rows = cur.fetchall()
-                all_stat_list = []
-                for row in rows:
-                    all_stat_list.append(row)
 
-                return all_stat_list
+                return rows
 
         except Exception as e:
             print(f"Error get_all_users: {e}")
             return []
     
-    def get_month_statistics(self):
+    def get_month_statistics(self, group_id):
         """ Get last month statistics """
         try:
             with self.db.conn.cursor() as cur:
@@ -64,19 +59,16 @@ class ScoreStatistics:
                 cur.execute(
                     """
                     SELECT user_id, SUM(point) as sum_point FROM scores
-                    WHERE date >= CURRENT_DATE - INTERVAL '1 month'
+                    WHERE group_id = %s and date >= CURRENT_DATE - INTERVAL '1 month'
                     GROUP BY user_id
                     ORDER BY sum_point desc
                     limit 5;
-                    """
+                    """, (group_id, ), 
                 )
 
                 rows = cur.fetchall()
-                all_stat_list = []
-                for row in rows:
-                    all_stat_list.append(row)
 
-                return all_stat_list
+                return rows
 
         except Exception as e:
             print(f"Error get_all_users: {e}")
